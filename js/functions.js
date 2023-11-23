49505b8fecf9371c02c5f06c352c909b891fcd7f
TrujiDev <eetgrisales@gmail.com>
2023-11-23 13:38:10 -0500

    Add showAlert function to display error messages

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+export function showAlert(msg) {
+	const alert = document.querySelector('.alert');
+
+	if (!alert) {
+		const alert = document.createElement('div');
+		alert.classList.add(
+			'bg-red-100',
+			'border-red-400',
+			'text-red-700',
+			'px-4',
+			'py-3',
+			'rounded',
+			'relative',
+			'max-w-lg',
+			'mx-auto',
+			'mt-6',
+			'text-center',
+			'alert'
+		);
+
+		alert.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block sm:inline">${msg}</span>
+        `;
+
+		const form = document.querySelector('#form');
+		form.appendChild(alert);
+
+		setTimeout(() => {
+			alert.remove();
+		}, 3000);
+	}
+}
