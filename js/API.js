df424db12aa61650a7a05b7535177d77870ffa83
TrujiDev <eetgrisales@gmail.com>
2023-11-23 13:55:26 -0500

    Add newCustomer function to create a new customer

--- js/API.js
+++ js/API.js
@@ -1,5 +1,10 @@
 const url = 'http://localhost:4000/customers';
 
+/**
+ * Sends a POST request to create a new customer.
+ * @param {Object} customer - The customer object to be created.
+ * @returns {Promise<void>} - A promise that resolves when the request is complete.
+ */
 export const newCustomer = async customer => {
 	try {
 		await fetch(url, {
@@ -8,8 +13,8 @@ export const newCustomer = async customer => {
 			headers: {
 				'Content-Type': 'application/json',
 			},
-        });
-        window.location.href = 'index.html';
+		});
+		window.location.href = 'index.html';
 	} catch (error) {
 		console.log(error);
 	}
