0dbf479b2077130ae2de22f84b0965d56784b696
TrujiDev <eetgrisales@gmail.com>
2023-11-23 13:38:21 -0500

    Add import statement for showAlert function and update validate function

--- js/newCustomer.js
+++ js/newCustomer.js
@@ -1,3 +1,5 @@
+import { showAlert } from './functions.js';
+
 (function () {
 	const form = document.querySelector('#form');
 
@@ -16,15 +18,20 @@
 			email,
 			phone,
 			company,
-        };
-        
-        if (validate(customer)) {
-            showAlert('All fields are required', 'error');
-            return;
-        }
+		};
+
+		if (validate(customer)) {
+			showAlert('All fields are required', 'error');
+			return;
+		}
 	}
 
+	/**
+	 * Validates an object by checking if all its values are not empty.
+	 * @param {Object} obj - The object to be validated.
+	 * @returns {boolean} - Returns true if any value in the object is empty, otherwise returns false.
+	 */
 	function validate(obj) {
-		return Object.values(obj).every(input => input !== '');
+		return !Object.values(obj).every(input => input !== '');
 	}
 })();
